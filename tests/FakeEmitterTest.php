171a1f42e4fabaaee20fe65617871687a8033652
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-05-14 17:01:59 +0300

    test FakeEmitter

---
+++ tests/FakeEmitterTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Yiisoft\PsrEmitter\FakeEmitter;
+
+use function PHPUnit\Framework\assertNull;
+use function PHPUnit\Framework\assertSame;
+
+final class FakeEmitterTest extends TestCase
+{
+    public function testBase(): void
+    {
+        $emitter = new FakeEmitter();
+
+        assertNull($emitter->getLastResponse());
+
+        $response1 = $this->createMock(ResponseInterface::class);
+        $emitter->emit($response1);
+
+        assertSame($response1, $emitter->getLastResponse());
+
+        $response2 = $this->createMock(ResponseInterface::class);
+        $emitter->emit($response2);
+
+        assertSame($response2, $emitter->getLastResponse());
+    }
+}
