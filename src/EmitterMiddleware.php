81fc7a44f281a4f6a160fbb7b7252c0e353e5fb6
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-05-14 16:55:27 +0300

    implement

---
+++ src/EmitterMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\PsrEmitter;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+final class EmitterMiddleware implements MiddlewareInterface
+{
+    public function __construct(
+        private EmitterInterface $emitter = new SapiEmitter(),
+    ) {
+    }
+
+    /**
+     * @throws HeadersHaveBeenSentException
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $response = $handler->handle($request);
+        $this->emitter->emit($response);
+        return $response;
+    }
+}
